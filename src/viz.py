a7108976e5c8c1842420a1e51f891c6610652862
Mayara Souza <mayara.ars1@gmail.com>
2023-02-14 16:00:33 +0100

    Source python files

--- src/viz.py
+++ src/viz.py
@@ -9,7 +9,7 @@ from plotly.subplots import make_subplots
 
 # Most mentioned places X Most views per place
 
-def general_data(year, places, option=2):
+def general_data(year, places):
 
     '''
     Function that returns a plot (bar and line) showing the number of mentions and the total views
@@ -22,16 +22,12 @@ def general_data(year, places, option=2):
     :returns:
         a fig 
     '''
-    #fig = make_subplots(rows=1, cols=2)
-
-    if option == 1:
-        top_views = places[places['year'] == year][:7]
-    else:
-        top_views = places[places['year'] == year][:5]    
+    
+    top_views = places[places['year'] == year][:5]    
     
     trace3 = go.Scatter(
                     mode="lines",
-                    x = top_views['top_places_'],
+                    x = top_views['places'],
                     y = top_views['mentions'],
                     name='Number of mentions',
                     marker = dict(color = 'black')
@@ -39,10 +35,10 @@ def general_data(year, places, option=2):
     )
     
     trace4 = go.Bar(
-                    x = top_views['top_places_'],
+                    x = top_views['places'],
                     y = top_views['total_number_of_views'],
                     name='Number of views',
-                    marker = dict(color = '#04cad8')
+                    marker = dict(color = '#cf5c49')
                     
     )
 
@@ -50,108 +46,110 @@ def general_data(year, places, option=2):
     
     fig.add_trace(trace3, secondary_y=True,)
     fig.add_trace(trace4)
-            
-    if option == 1:
-                fig['layout'].update(height = 700, width = 1200, 
-                                title = f"{year}'s top places in number of views <br><sup>Ranking order is not necessarily the same for mentions</sup>",
-                                xaxis=dict(tickangle=-45),
-                                xaxis_title="Top places",
-                                yaxis_title="Number of mentions",
-                                yaxis2_title="Total views in millions",
-                                plot_bgcolor='rgba(240, 242, 247, 0.8)',
-                                legend=dict(
-                                            yanchor="bottom",
-                                            y=0.8,
-                                            xanchor="right",
-                                            x=0.9
-                                        ))
-    else:
-        fig['layout'].update(height = 700, width = 1200, 
-                                title = f"{year}'s top places in number of views <br><sup>Ranking order is not necessarily the same for mentions</sup>",
-                                xaxis=dict(tickangle=0),
-                                xaxis_title="Top places",
-                                yaxis_title="Number of mentions",
-                                yaxis2_title="Total views in millions",
-                                plot_bgcolor='rgba(240, 242, 247, 0.8)',
-                                legend=dict(
-                                            yanchor="bottom",
-                                            y=0.8,
-                                            xanchor="right",
-                                            x=0.9
-                                        ))
+    
+    fig['layout'].update(height = 600, width = 1000, 
+                         title = f"{year}'s top 5 places in number of views <br><sup>Ranking order is not necessarily the same for mentions</sup>",
+                         xaxis=dict(tickangle=0),
+                         xaxis_title="Top 5 places",
+                         yaxis_title="Total number of views",
+                         yaxis2_title="Number of mentions",
+                         plot_bgcolor='rgba(240, 242, 247, 0.8)')
 
 
     return fig
 
     ##################################################
 
-# Videos mentioning a place X Google trends interest over time for the same place
 
-def plotting_trends_videos(trends, videos_info, place, year):
+def plotting_trends_videos(trends, trends_related, videos_info, place):
 
     '''
     Function that plots all the videos mentioning a certain place in a given year and
     the Google trends interest over time results for the same place in the same year.
 
     :params:
-        trends(df) = google trends df
-        videos_info(df) = a df created using the yt API with all the info about all the videos analysed (year published, view count, etc)
-        place(str) = the place to be plotted
-        year(int) = the year to be analysed
+        trends (df) = google trends df
+        trends_related (df) = google trends df with the "travel place" and "flight place" results
+        videos_info (df) = a df created using the yt API with all the info about all the videos analysed (year published, view count, etc)
+        place (str) = the place to be plotted
 
     :returns:
         a fig
     '''
-
+    
+    # selecting only the videos that mention the place being analyzed in the last 5 years
+    
     select = videos_info.loc[(videos_info['everywhere_string'].str.contains(f"{place}")) & 
-                             (videos_info['year_published'] == year)]
+                             (videos_info['year_published'] >= 2018)]
+    
+    # selecting the same place from the google trends df 
+    
+    select2 = trends[['date', f"{place}"]].loc[trends['date']<'2023-1-1']
+        
+    # selecting the results for the same place from the google trends related df (results for flight+place and travel+place) 
 
-    trace1 = go.Line(
-        x = trends['date'],
-        y = trends[place],
-        name= f'<b>Google trends results for {place}',
-        marker=dict(
-            color= 'black'#'rgb(34,163,192)'
-                   )
+    select3 = trends_related[["date", f"travel {place.lower()}", f"flight {place.lower()}"]].loc[trends_related['date']<'2023-1-1']
+    
+    trace1 = go.Scatter(
+        x = select2['date'],
+        y = select2[place],
+        name= f"''{place}''- Interest over time",
+        mode = 'lines'
     )
+    
     trace2 = go.Scatter(
+        x = select3['date'],
+        y = select3[f"travel {place.lower()}"],
+        name= f"''travel {place}''- Interest over time",
+        mode = 'lines'
+    )
+    
+    trace3 = go.Scatter(
+        x = select3['date'],
+        y = select3[f"flight {place.lower()}"],
+        name= f"''flight {place}'' - Interest over time",
+        mode = 'lines'
+    )
+    
+    trace4 = go.Scatter(
         x=select['publishedAt'],
         y=select['viewCount'],
-        name='Videos posted by <b> number of views',
+        name='Videos posted by number of views',
         yaxis='y2',
         mode = 'markers',
-        marker = dict(size=8,
-                      symbol = 'diamond-dot',
-                      color = '#04cad8')#'rgba(190, 167, 9, 0.8)')
-
+        marker = dict(size=4,
+                      color = '#60585a')
     )
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(trace1)
-    fig.add_trace(trace2, secondary_y=True)
+    fig.add_trace(trace2)
+    fig.add_trace(trace3)
+    fig.add_trace(trace4, secondary_y=True)
     
-    fig['layout'].update(#height = 600, width = 1000, 
-                         title = f"Google Trends results for '{place}' x <br>Views for videos that mention '{place}'",
-                         title_y = 0.99,
-                         title_x = 0.15,
+    fig['layout'].update(height = 600, width = 1000, 
+                         title = f"Google Trends results for '{place}' x Views on YouTube for videos that mention '{place}'",
                          xaxis=dict(tickangle=-45),
-                        #xaxis_title="Date",
-                        yaxis_title="Google trends - interest over time",
-                        yaxis2_title="Number of views per video in millions",
-                        plot_bgcolor='rgba(240, 242, 247, 0.8)',
-                        legend=dict(
-                                    #yanchor="bottom",
-                                    y=-0.5,
-                                    #xanchor="left",
-                                    x=-0.1,
-                                    font_size = 10
-
-                                ),
-                        font = dict(size=10))
-                        #,legend_title="Legend")
+                         xaxis_title="Date",
+                         yaxis_title="Google trends - interest over time",
+                         yaxis2_title="Number of views per video",
+                         plot_bgcolor= 'rgba(240, 242, 247, 0.8)')
     
-    fig.update_xaxes(minor=dict(ticklen=6, tickcolor="black", showgrid=True))
-    fig.update_layout(width=1100,height=900)
+    fig.update_xaxes(minor=dict(ticklen=6, tickcolor="black", showgrid=True),
+                     rangeslider_visible=True,
+                     rangeselector=dict(
+                     buttons=list([
+                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                        dict(count=6, label="6m", step="month", stepmode="backward"),
+                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                        dict(step="all")
+                    ])
+    
+    ),
+        rangeslider_thickness = 0.15,
+        range = ('2018-01-01', '2022-12-01'),
+        rangeslider_range = ('2018-01-01', '2022-12-01')
+    )
     
     return fig
     
@@ -159,7 +157,7 @@ def plotting_trends_videos(trends, videos_info, place, year):
 
 # Number of views per place over time X Number of tourist arrivals over time
 
-def plotting_tourism_data (places_mentions_views, number_of_tourist_arrivals, place):
+def plotting_tourism_data (places_mentions_views, tourism_info, place):
 
     '''
     Function that plots the video info (total number of views) and number of tourist arrivals
@@ -168,15 +166,13 @@ def plotting_tourism_data (places_mentions_views, number_of_tourist_arrivals, pl
     :params:
         places_mentions_views(df) = a df with all the places mentioned in all the videos, the total number of mentions
         and the total number of views for each along the years
-        tourism_info(df) = df with the number of tourist arrivals for many different countries
+        tourism_info(df) = df with the number of tourist arrivals for many different places
     
     :returns:
         a fig
     '''
-    
-    #place = input('Type the place \n')
-    
-    views = places_mentions_views.loc[(places_mentions_views['top_places_']==place) & ((places_mentions_views['year']>=2018))]
+
+    views = places_mentions_views.loc[(places_mentions_views['places']==place) & (places_mentions_views['year']>=2018)]
     
     arrivals = number_of_tourist_arrivals.loc[(number_of_tourist_arrivals['Entity'] == place) 
                                              & (number_of_tourist_arrivals['Year']>=2018)]
@@ -185,15 +181,15 @@ def plotting_tourism_data (places_mentions_views, number_of_tourist_arrivals, pl
     fig = make_subplots(#rows=1, cols=2, 
                     specs=[[{"secondary_y": True}]])    
     
-    trace1 = go.Line(
-                    #mode = 'lines',
+    trace1 = go.Scatter(
+                    mode = 'lines',
                     x = views['year'],
                     y = views['total_number_of_views'],
-                    name= f'Total number of views',
+                    name= f'Total number of YouTube views for {place}',
                     yaxis='y2',
                     line = dict(width = 2),
                     marker=dict(
-                                color='black',#rgb(34,163,192)
+                                color='black'
                                 line = dict(width = 2, color='black')
                                 )
     )
@@ -202,7 +198,7 @@ def plotting_tourism_data (places_mentions_views, number_of_tourist_arrivals, pl
                     x = arrivals['Year'],
                     y = arrivals['International tourism, number of arrivals'],
                     name='Number of tourists arriving',
-                    marker = dict(color = '#04cad8')#rgba(221, 206, 103, 0.8)
+                    marker = dict(color = '#cf5c49')
     )
 
     
@@ -211,51 +207,72 @@ def plotting_tourism_data (places_mentions_views, number_of_tourist_arrivals, pl
     
 
     fig['layout'].update(height = 600, width = 1000, 
-                         title = f"Tourist arrivals in '{place}' x <br>Views for videos mentioning '{place}'",
+                         title = f"Tourist arrivals in '{place}' x Views on YouTube for videos mentioning '{place}'",
                          xaxis=dict(tickangle=0, type = 'category', categoryorder='array', 
-                                    categoryarray = ['2018', '2019', '2020', '2021']),
+                                    categoryarray = ['2018', '2019', '2020', '2021', '2022']),
                          xaxis_title="Year",
                          yaxis2_title="Total views in millions",
                          yaxis_title="Number of arrivals in millions",
-                         plot_bgcolor='rgba(240, 242, 247, 0.8)',
-                         legend=dict(
-                                    #yanchor="bottom",
-                                    y=-0.5,
-                                    #xanchor="left",
-                                    x=-0.1,
-                                    font_size = 10
-                                ))
-                        #,legend_title="Legend")
+                         plot_bgcolor='rgba(240, 242, 247, 0.8)')
+                        
 
     fig.update_yaxes(minor=dict(ticklen=6, tickcolor="black", showgrid=True, nticks=3))
     
 
     return fig
-    ##################################################
-
-def correlation_heatmap(merged_df, place):
-    
-    x_cor = merged_df[['total_number_of_views', 'tourist_arrivals', 'google_trends']].corr()
-
-    fig=go.Figure()
-
-    fig.add_trace(go.Heatmap(
-    x = x_cor.columns,
-    y = x_cor.index,
-    z = np.array(x_cor),
-    colorscale=px.colors.diverging.RdBu,
-    zmin=-1,
-    zmax=1
-    ))
-
-    fig['layout'].update(height = 600, width = 1000, 
-                         title = f"Correlation between '{place}' videos x <br>Google trends results x Tourist arrivals",
-                         title_x = 0.5,
-                         plot_bgcolor='rgba(240, 242, 247, 0.8)')
-                         
-
-    return fig
 
     ##################################################
 
+    def sns_correlation_heatmap(merged_df, place, years = 'all'):
     
+    # checking the correlation in the 5 last years
+    if years == 'all':
+        
+        corr = merged_df[['Total_Weekly_Views', f'{place}', f'Flight {place}', f'Travel {place}']].corr()
+
+        # Getting the Upper Triangle of the co-relation matrix
+        matrix = np.triu(corr)
+
+        # using the upper triangle matrix as mask 
+        sns.heatmap(corr, annot=True, mask=matrix, cmap = sns.light_palette("#cf5c49", as_cmap=True))
+        plt.xticks(rotation=-45)
+        plt.suptitle(f'Correlation between Google Trends results and \nweekly YouTube views for videos mentioning {place}',
+                     fontsize = 12, y = 1.1)
+    
+    # checking the correlation only in the years in which the place was the most viewed
+    else:
+        
+        corr = merged_df[merged_df['Year_Published'].isin(years)]
+        corr = corr[['Total_Weekly_Views', f'{place}', f'Flight {place}', f'Travel {place}']].corr()
+
+        # Getting the Upper Triangle of the co-relation matrix
+        matrix = np.triu(corr)
+
+        # using the upper triangle matrix as mask 
+        sns.heatmap(corr, annot=True, mask=matrix, cmap = sns.light_palette("#cf5c49", as_cmap=True))
+        plt.xticks(rotation=-45)
+        plt.suptitle(f'Correlation between Google Trends results and \nweekly YouTube views for videos mentioning {place} in {years}',
+                     fontsize = 12, y = 1.1)
+    
+    ##################################################
+
+    def correlation_heatmap_tourism(tourism_df, views_df, place):
+    
+    select1 = views_df[views_df['places']==place]
+    select2 = tourism_df[tourism_df['Entity']==place]
+    
+    corr = select1.merge(select2, left_on='year', right_on='Year')[
+            ['total_number_of_views', 'International tourism, number of arrivals']].corr()
+
+    
+
+    # Getting the Upper Triangle of the co-relation matrix
+    matrix = np.triu(corr)
+    
+    # using the upper triangle matrix as mask 
+    sns.heatmap(corr, annot=True, mask=matrix, cmap = sns.light_palette("#cf5c49", as_cmap=True), 
+                yticklabels=False, xticklabels=False, cbar=False)
+    
+    plt.xticks(rotation=-45)
+    plt.suptitle(f'Correlation between tourist arrivals \nper year and YouTube views for \nvideos mentioning {place}',
+                 fontsize = 12, y = 0.8, x = 0.34, ha='center')
